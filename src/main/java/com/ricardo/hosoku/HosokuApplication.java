9f2141caba02931c20aca1187aa4ca7521149e7c
RicardoVercet <ricardobieb01@gmail.com>
2025-02-07 18:21:08 +0530

    project initialized

---
+++ src/main/java/com/ricardo/hosoku/HosokuApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.hosoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosokuApplication.class, args);
+	}
+
+}
